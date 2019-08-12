d03d26b8f2a290cab40b4002001d7f139bcf2456
ashiqks <ashiqgiga07@gmail.com>
2019-08-12 14:46:18 +0400

    Never Ever Give Up

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello():
-    return "Hello World! Travis is AWESOME"
+    return "Hello World! Travis is SUPER AWESOME"
 
 if __name__ == '__main__':
     application.run()
